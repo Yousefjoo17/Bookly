030c5780101176acab5ffc10072c9298ef26f0a6
Youssef <yousefyousefyousef335@gmail.com>
2023-10-06 18:02:22 +0300

    best seller item done

--- lib/features/Home/presentation/views/widgets/Best_seller_list_item.dart
+++ lib/features/Home/presentation/views/widgets/Best_seller_list_item.dart
@@ -1,6 +1,7 @@
 import 'package:bookly/constans.dart';
 import 'package:bookly/core/utils/assets.dart';
 import 'package:bookly/core/utils/styles.dart';
+import 'package:bookly/features/Home/presentation/views/widgets/book_rating.dart';
 import 'package:flutter/material.dart';
 
 class BestSellersListItem extends StatelessWidget {
@@ -25,37 +26,41 @@ class BestSellersListItem extends StatelessWidget {
             ),
           ),
           const SizedBox(width: 30),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                width: MediaQuery.of(context).size.width * .5,
-                child: Text(
-                  'Harry Potter and the Goblet of Fire',
-                  style: Styles.textStyle20.copyWith(
-                    fontFamily: kGTSectraFine,
-                  ),
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                ),
-              ),
-              const SizedBox(height: 3),
-              const Text(
-                'J.K. Rowling',
-                style: Styles.textStyle14,
-              ),
-              const SizedBox(height: 3),
-              Row(
-                children: [
-                  Text(
-                    '19.99 €',
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * .5,
+                  child: Text(
+                    'Harry Potter and the Goblet of Fire',
                     style: Styles.textStyle20.copyWith(
-                      fontWeight: FontWeight.bold,
+                      fontFamily: kGTSectraFine,
                     ),
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
                   ),
-                ],
-              )
-            ],
+                ),
+                const SizedBox(height: 3),
+                const Text(
+                  'J.K. Rowling',
+                  style: Styles.textStyle14,
+                ),
+                const SizedBox(height: 3),
+                Row(
+                  children: [
+                    Text(
+                      '19.99 €',
+                      style: Styles.textStyle20.copyWith(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                   const Spacer(),
+                   const BookRating(),
+                  ],
+                )
+              ],
+            ),
           ),
         ],
       ),
