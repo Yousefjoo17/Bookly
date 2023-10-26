268d3c7477c4f4023c232148982d2f8c0830e2d8
Youssef <yousefyousefyousef335@gmail.com>
2023-10-26 23:41:10 +0200

    either calss

--- lib/features/Home/data/repos/home_repo.dart
+++ lib/features/Home/data/repos/home_repo.dart
@@ -1,5 +1,8 @@
+import 'package:bookly/core/errors/failure.dart';
+import 'package:bookly/features/Home/data/models/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo {
-  //here I say what the feature does not how it does it
-  fetchFeaturedBooks();
-  fetchBestSellerBooks();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks();
 }
