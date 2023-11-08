ba737c934d5d85dc42cd6c0ef3a0b414a45c62bb
Youssef <yousefyousefyousef335@gmail.com>
2023-11-08 02:42:10 +0200

    featchFeaturedBooks

--- lib/features/Home/data/repos/home_repo_impl.dart
+++ lib/features/Home/data/repos/home_repo_impl.dart
@@ -13,7 +13,7 @@ class HomeRepoImpl implements HomeRepo {
   Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() async {
     try {
       var data = await apiService.get(
-        endPoint: "volumes?Filtering=free-ebooks&q=subjectProgramming",
+        endPoint: "volumes?Filtering=free-ebooks&q=Programming",
       );
       List<BookModel> books = [];
       for (var item in data["items"]) {
@@ -29,7 +29,20 @@ class HomeRepoImpl implements HomeRepo {
   }
 
   @override
-  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
-    throw UnimplementedError();
-  }
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() async{
+try {
+      var data = await apiService.get(
+        endPoint: "volumes?Filtering=free-ebooks&q=algorithm",
+      );
+      List<BookModel> books = [];
+      for (var item in data["items"]) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioexception(e));
+      }
+      return left(ServerFailure(errMessage: e.toString()));
+    }  }
 }
