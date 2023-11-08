a19052adbe079b9f5b6712713b324055dd0f16ee
Youssef <yousefyousefyousef335@gmail.com>
2023-11-08 02:29:39 +0200

    create server failure

--- lib/features/Home/data/repos/home_repo_impl.dart
+++ lib/features/Home/data/repos/home_repo_impl.dart
@@ -3,6 +3,7 @@ import 'package:bookly/core/utils/api_service.dart';
 import 'package:bookly/features/Home/data/models/book_model/book_model.dart';
 import 'package:bookly/features/Home/data/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImpl implements HomeRepo {
   final ApiService apiService;
@@ -14,14 +15,16 @@ class HomeRepoImpl implements HomeRepo {
       var data = await apiService.get(
         endPoint: "volumes?Filtering=free-ebooks&q=subjectProgramming",
       );
-
       List<BookModel> books = [];
       for (var item in data["items"]) {
         books.add(BookModel.fromJson(item));
       }
       return right(books);
     } catch (e) {
-      return left(ServerFailure());
+      if (e is DioException) {
+        return left(ServerFailure.fromDioexception(e));
+      }
+      return left(ServerFailure(errMessage: e.toString()));
     }
   }
 
