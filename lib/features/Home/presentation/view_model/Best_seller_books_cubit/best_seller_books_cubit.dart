68b6d68dc3c9608db74aadee003f7ba324459c8f
Youssef <yousefyousefyousef335@gmail.com>
2023-11-11 20:11:55 +0200

    fetch best seller books

---
+++ lib/features/Home/presentation/view_model/Best_seller_books_cubit/best_seller_books_cubit.dart
@@ -0,0 +1,20 @@
+import 'package:bloc/bloc.dart';
+import 'package:bookly/features/Home/data/models/book_model/book_model.dart';
+import 'package:bookly/features/Home/data/repos/home_repo.dart';
+import 'package:equatable/equatable.dart';
+
+part 'best_seller_books_state.dart';
+
+class BestSellerBooksCubit extends Cubit<BestSellerBooksState> {
+  BestSellerBooksCubit(this.homeRepo) : super(BestSellerBooksInitial());
+  final HomeRepo homeRepo;
+  Future<void> fetchBestSellerBooks() async {
+    emit(BestSellerBooksLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold((booksfailure) {
+      emit(BestSellerBooksFailure(errMessage: booksfailure.errMessage));
+    }, (books) {
+      emit(BestSellerBooksSuccess(books));
+    });
+  }
+}
---
+++ lib/features/Home/presentation/view_model/Best_seller_books_cubit/best_seller_books_state.dart
@@ -0,0 +1,22 @@
+part of 'best_seller_books_cubit.dart';
+
+sealed class BestSellerBooksState extends Equatable {
+  const BestSellerBooksState();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class BestSellerBooksInitial extends BestSellerBooksState {}
+
+final class BestSellerBooksLoading extends BestSellerBooksState {}
+
+final class BestSellerBooksSuccess extends BestSellerBooksState {
+  final List<BookModel> booksList;
+  const BestSellerBooksSuccess(this.booksList);
+}
+
+final class BestSellerBooksFailure extends BestSellerBooksState {
+  final String errMessage;
+  const BestSellerBooksFailure({required this.errMessage});
+}
