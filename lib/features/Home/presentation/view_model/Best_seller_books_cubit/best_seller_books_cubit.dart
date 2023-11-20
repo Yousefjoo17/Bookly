16a7797b7772d63733cebb72ec946e1c40755c2e
Youssef <yousefyousefyousef335@gmail.com>
2023-11-20 02:24:49 +0200

    setup Serivce locator

--- lib/features/Home/presentation/view_model/Best_seller_books_cubit/best_seller_books_cubit.dart
+++ lib/features/Home/presentation/view_model/Best_seller_books_cubit/best_seller_books_cubit.dart
@@ -2,12 +2,12 @@ import 'package:bloc/bloc.dart';
 import 'package:bookly/features/Home/data/models/book_model/book_model.dart';
 import 'package:bookly/features/Home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
-
 part 'best_seller_books_state.dart';
 
 class BestSellerBooksCubit extends Cubit<BestSellerBooksState> {
   BestSellerBooksCubit(this.homeRepo) : super(BestSellerBooksInitial());
   final HomeRepo homeRepo;
+  //final HomeRepoImpl homeRepoImpl;
   Future<void> fetchBestSellerBooks() async {
     emit(BestSellerBooksLoading());
     var result = await homeRepo.fetchFeaturedBooks();
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:bookly/core/utils/app_router.dart';
+import 'package:bookly/core/utils/service_locator.dart';
+import 'package:bookly/features/Home/data/repos/home_repo_impl.dart';
+import 'package:bookly/features/Home/presentation/view_model/Best_seller_books_cubit/best_seller_books_cubit.dart';
+import 'package:bookly/features/Home/presentation/view_model/featured_books_cubit/featured_books_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
+  setupServiceLocator();
   runApp(const BooklyApp());
 }
 
@@ -12,15 +18,29 @@ class BooklyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: AppRouter.router,
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: const Color(0xff100B20),
-        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => BestSellerBooksCubit(
+            getIt.get<HomeRepoImpl>(),
+          ),
+        ),
+        BlocProvider(
+          create: (context) => FeaturedBooksCubit(
+            getIt.get<HomeRepoImpl>(),
+          ),
+        ),
+      ],
+      child: MaterialApp.router(
+        routerConfig: AppRouter.router,
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: const Color(0xff100B20),
+          textTheme:
+              GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+        ),
+        title: 'Bookly',
       ),
-      title: 'Bookly',
     );
   }
 }
-
