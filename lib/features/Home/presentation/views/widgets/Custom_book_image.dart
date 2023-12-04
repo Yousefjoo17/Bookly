54ee6bc0c7a570c97959f0a99571c245236125ed
Youssef <yousefyousefyousef335@gmail.com>
2023-12-04 21:31:43 +0200

    cached network image

--- lib/features/Home/presentation/views/widgets/Custom_book_image.dart
+++ lib/features/Home/presentation/views/widgets/Custom_book_image.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class CustomBookImage extends StatelessWidget {
@@ -5,19 +6,20 @@ class CustomBookImage extends StatelessWidget {
   final String imagrUrl;
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 1.35 / 2, // width : height
-      child: Container(
-// height: MediaQuery.of(context).size.width * 0.5,
-// width: MediaQuery.of(context).size.width * 0.3,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(12),
-          image: DecorationImage(
-            image: NetworkImage(imagrUrl),
-            fit: BoxFit.fill,
-          ),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(16),
+      child: AspectRatio(
+        aspectRatio: 1.35 / 2, // width : height
+        child: CachedNetworkImage(
+          imageUrl: imagrUrl,
+          fit: BoxFit.fill,
+          errorWidget: (context, url, error) =>
+              const Icon(Icons.error, size: 60, color: Colors.red),
+              placeholder: (context, url) => const Center(child: CircularProgressIndicator()),
         ),
       ),
     );
   }
 }
+// height: MediaQuery.of(context).size.width * 0.5,
+// width: MediaQuery.of(context).size.width * 0.3,
