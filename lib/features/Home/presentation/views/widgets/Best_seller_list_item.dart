32fb0575072b2af089b3e161c2e75db4bbdffe5f
Youssef <yousefyousefyousef335@gmail.com>
2024-01-26 09:25:49 +0200

    fetch similar books

--- lib/features/Home/presentation/views/widgets/Best_seller_list_item.dart
+++ lib/features/Home/presentation/views/widgets/Best_seller_list_item.dart
@@ -15,7 +15,7 @@ class BestSellersListItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
-        GoRouter.of(context).push(AppRouter.kBookDetailsView);
+        GoRouter.of(context).push(AppRouter.kBookDetailsView,extra: bookModel);
       },
       child: SizedBox(
         height: 120,
--- lib/features/Home/presentation/views/widgets/similar_books_list_view.dart
+++ lib/features/Home/presentation/views/widgets/similar_books_list_view.dart
@@ -1,23 +1,39 @@
+import 'package:bookly/core/widgets/custom_loading_indicator.dart';
+import 'package:bookly/core/widgets/custome_error_widget.dart';
+import 'package:bookly/features/Home/presentation/view_model/similar_books_cubit/similar_books_cubit.dart';
 import 'package:bookly/features/Home/presentation/views/widgets/Custom_book_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SimilarBooksListView extends StatelessWidget {
   const SimilarBooksListView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * .14,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemCount: 12,
-        itemBuilder: (context, index) {
-          return const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 6.0),
-            child: CustomBookImage(imagrUrl: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"),
+    return BlocBuilder<SimilarBooksCubit, SimilarBooksState>(
+      builder: (context, state) {
+        if (state is SimilarBooksSuccess) {
+          return SizedBox(
+            height: MediaQuery.of(context).size.height * .14,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: 12,
+              itemBuilder: (context, index) {
+                return const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 6.0),
+                  child: CustomBookImage(
+                      imagrUrl:
+                          "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"),
+                );
+              },
+            ),
           );
-        },
-      ),
+        } else if (state is SimilarBooksFailure) {
+          return CustomErrorWidget(errMessage: state.errMessage);
+        } else {
+          return const CustomloadingIndicatorWidget();
+        }
+      },
     );
   }
 }
